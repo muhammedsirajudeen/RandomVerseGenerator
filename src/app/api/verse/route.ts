e8c08ebec2cd1a82bbc13a94a5190cefe1d8adc7
muhammedsirajudeen <muhammedsirajudeen29@gmail.com>
2024-11-25 16:24:48 +0530

    Feat: fixed linter issue

--- src/app/api/verse/route.ts
+++ src/app/api/verse/route.ts
@@ -13,9 +13,7 @@ export const axiosInstance=axios.create(
 )
 axiosRetry(axiosInstance, { retries: 10, retryDelay: axiosRetry.exponentialDelay });
 
-export async function GET(request: NextRequest):Promise<NextResponse> {
-    const url = request.nextUrl
-    const verse = url.searchParams.get('verse')
+export async function GET():Promise<NextResponse> {
     const jsonData = await axiosInstance.get(argInjector(Math.floor(Math.random() * 6236) + 1, "en.asad"))
     const arabicjsonData = await axiosInstance.get(argInjector(Math.floor(Math.random() * 6236) + 1, "ar"))
     return NextResponse.json({ englishresponse: jsonData.data , arabicresponse:arabicjsonData.data })
